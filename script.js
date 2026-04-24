c50a00d8e5f4a259a2ad4f98dc38d569426a2987
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-04-24 12:59:30 -0700

    review part 1

---
+++ script.js
@@ -0,0 +1,27 @@
+// weather Api:
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code,relative_humidity_2m,apparent_temperature,wind_speed_10m,precipitation&past_days=0&forecast_days=7
+
+// geocode/geolocation to find longitude and latitude
+// End Point
+//  https://nominatim.openstreetmap.org/search?
+//  https://nominatim.openstreetmap.org/search?q=birmingham%2C+pilkington+avenue&format=jsonv2
+
+async function getData() {
+  let search = "los angeles, ca"
+  const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getData();
+
