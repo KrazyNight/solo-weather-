a63f65402b439d9d62ac5af74954c0cb53314dd3
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-04-28 12:57:25 -0700

    review daily weather code part 2

--- script.js
+++ script.js
@@ -574,6 +574,30 @@
 
 
 
+// example 3: complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+
+
+
+
+
+
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
+//complete
 
 
 
@@ -583,11 +607,169 @@
 
 
 
+//example 3: woring on daily
+
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+
+
+//
+// const ddlUnits = document.querySelector("#ddlUnits");
+
+// const dvCityCountry = document.querySelector("#dvCityCountry");
+// const dvCurrDate = document.querySelector("#dvCurrDate");
+// const dvCurrTemp = document.querySelector("#dvCurrTemp");
+
+// const pFeelsLike = document.querySelector("#pFeelsLike");
+// const pHumidity = document.querySelector("#pHumidity");
+// const pWind = document.querySelector("#pWind");
+// const pPrecipitation = document.querySelector("#pPrecipitation");
 
 
 
 
-//continue 
+// let cityName, countryName;
+
+
+
+
+
+// async function getGeoData() {
+//   let search = "los angeles, ca"
+
+//   const url = `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2&addressdetails=1`;
+
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+
+
+//     let lat = result[0].lat;
+//     let lon = result[0].lon;
+
+//     loadLocationData(result)
+//     getWeatherData(lat, lon);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+// function loadLocationData(locationData){
+//   let location = locationData[0].address;
+//   cityName = location.city;
+//   countryName = location.country_code.toUpperCase();
+
+
+//   const today = new Date();
+
+//   const dateOptions = { 
+//     weekday: 'long', 
+//     year: 'numeric', 
+//     month: 'short', 
+//     day: 'numeric' 
+//   };
+
+//   const formattedDate = today.toLocaleDateString('en-US', dateOptions);
+
+//   dvCityCountry.textContent = `${cityName}, ${countryName}`;
+//   dvCurrDate.textContent = formattedDate;
+
+//   //console.log(formattedDate); 
+//   //Output Example: "Tuesday, Aug 5, 2025"
+//   //console.log(cityName, countryName, formattedDate)
+
+// }
+
+
+// async function getWeatherData(lat, lon) {
+//   let tempUnit = "celsius";
+//   let windUnit = "kmh";
+//   let precipUnit = "mm";
+
+//   if (ddlUnits.value === "F") {
+//     tempUnit = "fahrenheit";
+//     windUnit = "mph";
+//     precipUnit = "inch";
+//   }
+
+
+//   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code,relative_humidity_2m,apparent_temperature,wind_speed_10m,precipitation&past_days=0&forecast_days=7&wind_speed_unit=${windUnit}&temperature_unit=${tempUnit}&precipitation_unit=${precipUnit}`;
+
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+
+//     loadCurrentWeather(result); // why do I place this function here
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+// function loadCurrentWeather(weatherData) {
+//   dvCurrTemp.textContent = Math.round(weatherData.current.temperature_2m);
+//   pFeelsLike.textContent = Math.round(weatherData.current.apparent_temperature);
+//   pHumidity.textContent = weatherData.current.relative_humidity_2m;
+//   pWind.textContent = `${weatherData.current.wind_speed_10m} ${weatherData.current_units.wind_speed_10m.replace("mp/h", "mph")}`;
+//   pPrecipitation.textContent = `${weatherData.current.precipitation} ${weatherData.current_units.precipitation.replace("inch", "in")}`;
+// };
+
+
+
+
+
+
+// getGeoData();
+
+// ddlUnits.addEventListener("change", getGeoData);
+
+
+
+// template above  #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template #3
+// template above #3
+
+
+
+
+//continue
+//create function getWeatherFileName(){}
+//create function loadDailyForecast(){}
+// ----- create week loop 
+//----get date; number 1
+// ----- get dayOfWeek = mon, thu,  wed
+
+
+
+
 const ddlUnits = document.querySelector("#ddlUnits");
 
 const dvCityCountry = document.querySelector("#dvCityCountry");
@@ -683,13 +865,13 @@ async function getWeatherData(lat, lon) {
     const result = await response.json();
     console.log(result);
 
-    loadWeatherData(result); // why do I place this function here
+    loadCurrentWeather(result); // why do I place this function here
   } catch (error) {
     console.error(error.message);
   }
 }
 
-function loadWeatherData(weatherData) {
+function loadCurrentWeather(weatherData) {
   dvCurrTemp.textContent = Math.round(weatherData.current.temperature_2m);
   pFeelsLike.textContent = Math.round(weatherData.current.apparent_temperature);
   pHumidity.textContent = weatherData.current.relative_humidity_2m;
@@ -697,7 +879,56 @@ function loadWeatherData(weatherData) {
   pPrecipitation.textContent = `${weatherData.current.precipitation} ${weatherData.current_units.precipitation.replace("inch", "in")}`;
 };
 
+function getWeatherFileName(code) {
+  // sunny - 0, 
+  // partly-cloudy - 1,2
+  // overcast - 3
+  // fog - 45, 48
+  // drizzle -51, 53, 55, 56, 57
+  // rain - 61, 63, 65, 66, 67, 80, 81, 82
+  // snow - 71, 73, 75, 77, 85, 86
+  // storm -95, 96, 99
 
+  const weatherCodes = {
+    0: "sunny",
+    1: "partly-cloudy",
+    2: "partly-cloudy",
+    3: "overcast",
+    45: "fog",
+    48: "fog",
+    51: "drizzle",
+    53: "drizzle",
+    55: "drizzle",
+    56: "drizzle",
+    57: "drizzle",
+    61: "rain",
+    63: "rain",
+    65: "rain",
+    66: "rain",
+    67: "rain",
+    80: "rain",
+    81: "rain",
+    82: "rain",
+    71: "snow",
+    73: "snow",
+    75: "snow",
+    77: "snow",
+    85: "snow",
+    86: "snow",
+    95: "storm",
+    96: "storm",
+    99: "storm",
+  };
+
+  let fileName = `icon-${weatherCodes[code]}.webp`;
+
+  return fileName;
+  
+  
+}
+
+
+//console.log(getWeatherFileName(1))
 
 
 
@@ -705,4 +936,3 @@ function loadWeatherData(weatherData) {
 getGeoData();
 
 ddlUnits.addEventListener("change", getGeoData);
-
